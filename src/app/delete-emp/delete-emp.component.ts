16feecd1385128349ca378a59f78dfff159d9fbe
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-24 16:48:57 +0530

    delete employee

---
+++ src/app/delete-emp/delete-emp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-emp',
+  templateUrl: './delete-emp.component.html',
+  styleUrls: ['./delete-emp.component.css']
+})
+export class DeleteEmpComponent {
+
+}
