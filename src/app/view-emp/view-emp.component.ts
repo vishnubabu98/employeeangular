d27053b07f2c42625d0394e53d75e157f0642bfd
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-25 11:34:46 +0530

    add data binding concepts

--- src/app/view-emp/view-emp.component.ts
+++ src/app/view-emp/view-emp.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-emp.component.css']
 })
 export class ViewEmpComponent {
+  data:any= [
+    {
+      "name":"vishnu",
+      "designation":"software engineer trainee",
+      "salary":25000,
+      "company":"NeST"
+  
+    },
+    {
+      "name":"vivek",
+      "designation":"Tester",
+      "salary":25000,
+      "company":"UST"
+  
+    },
+    {
+      "name":"Jibu",
+      "designation":"System Engineer",
+      "salary":25000,
+      "company":"TCS"
+  
+    },
+    {
+      "name":"Abhijith",
+      "designation":"Network Engineer",
+      "salary":25000,
+      "company":"Orion"
+  
+    },
+    {
+      "name":"Shayi",
+      "designation":"software engineer",
+      "salary":65000,
+      "company":"Google"
+  
+    },
+    {
+      "name":"Anu",
+      "designation":"software Engineer",
+      "salary":65000,
+      "company":"PITS"
+  
+    }
+  ]
 
 }
